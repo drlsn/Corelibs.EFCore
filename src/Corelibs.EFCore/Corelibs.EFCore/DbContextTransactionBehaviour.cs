eb9f284a5fb537074d8264c96e7960a478592415
drlsn <dariusz.lusnia@gmail.com>
2023-06-21 15:52:37 +0200

    Add files

---
+++ src/Corelibs.EFCore/Corelibs.EFCore/DbContextTransactionBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using Corelibs.Basic.Blocks;
+using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Corelibs.EFCore
+{
+    public class DbContextTransactionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        private readonly DbContext _context;
+
+        public DbContextTransactionBehaviour(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask<TResponse> Handle(TRequest command, CancellationToken ct, MessageHandlerDelegate<TRequest, TResponse> next)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var entries = _context.ChangeTracker.Entries().ToArray();
+                    var response = await next(command, ct);
+                    if (response is Result result && !result.IsSuccess)
+                        return response;
+
+                    bool isCommand = typeof(TRequest).GetInterface(typeof(IBaseCommand).Name) != null;
+                    if (isCommand)
+                        await transaction.CommitAsync();
+
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    //await transaction.RollbackAsync();
+                    Console.WriteLine(ex.ToString());
+                    throw ex;
+                    return default;
+                }
+            }
+        }
+    }
+}
---
+++ src/Corelibs.EFCore/Corelibs.EFCore/DbContextTransactionDecorator.cs
@@ -0,0 +1,40 @@
+﻿using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Corelibs.EFCore
+{
+    public class DbContextTransactionDecorator<TCommand, TResponse> : ICommandHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+    {
+        private readonly ICommandHandler<TCommand, TResponse> _decorated;
+        private readonly DbContext _context;
+
+        public DbContextTransactionDecorator(ICommandHandler<TCommand, TResponse> decorated, DbContext context)
+        {
+            _decorated = decorated;
+            _context = context;
+        }
+
+        public async ValueTask<TResponse> Handle(TCommand command, CancellationToken ct)
+        {
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var response = await _decorated.Handle(command, ct);
+
+                    await transaction.CommitAsync();
+
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    Console.WriteLine(ex.ToString());
+
+                    return default;
+                }
+            }
+        }
+    }
+}
